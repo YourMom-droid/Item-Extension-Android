9ce980c559801b0750c3524e80d59a59c5e14171
YourMom-droid <hamdudumiedie@gmail.com>
2025-02-14 09:32:11 +0700

    Update Selling.cs

--- Patches/ShopMenu/Selling.cs
+++ Patches/ShopMenu/Selling.cs
@@ -87,7 +87,11 @@ public partial class ShopMenuPatches
 
                 __instance.currentItemIndex =
                     Math.Max(0, Math.Min(__instance.forSale.Count - 4, __instance.currentItemIndex));
-                __instance.updateSaleButtonNeighbors();
+                var method = __instance.GetType().GetMethod("updateSaleButtonNeighbors");
+                if (method != null)
+                {
+                    method.Invoke(__instance, null);
+                }
                 Reflection.GetMethod(__instance, "setScrollBarToCurrentIndex").Invoke();
                 return false;
             }
